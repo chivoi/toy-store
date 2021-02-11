498f539aa4d83c3b4e3cf1d125719510cf2e719f
chivoi <a.lastovirya@gmail.com>
2021-02-11 11:42:33 +1100

    sets up Feature and Listing model & seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = ["Soft Toy", "Board Game", "Figurine", "Puzzle", "Vehicle", "Card Game", "Book", "Block and Brick"]
+features = ["Educational", "Baby Safe", "Collectable", "Interactive", "Companion App", "Environmentally Friendly"]
+
+if Category.count == 0
+  categories.each do |category|
+    Category.create(name: category)
+    puts "created #{category} category"
+  end
+end
+
+if Feature.count == 0
+  features.each do |feature|
+    Feature.create(name: feature)
+    puts "created #{feature} feature"
+  end
+end
